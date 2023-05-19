9f12b16036f9c7b98f72c0cccccdfaa646b3b196
wata41 <130279514+wata41@users.noreply.github.com>
2023-05-19 09:14:06 +0000

    7.1-7.11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+    # GET /users/:id
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
+      # => GET app/views/users/new.html.erb
+  end
+
+  # GET /users/new
   def new
       # => GET app/views/users/new.html.erb
   end
