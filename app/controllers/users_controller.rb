c2a565f36e8090d7fea36a4d993b240571d57b74
wata41 <130279514+wata41@users.noreply.github.com>
2023-05-19 10:31:54 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      # => Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # => GET /users/:id
+      # redirect_to user_path(@user)    # => GET /users/:id
+      # redirect_to user_path(@user.id) # => GET /users/2
     else
+      # => Failure
       render 'new', status: :unprocessable_entity
     end
   end
