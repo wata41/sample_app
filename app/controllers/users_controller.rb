7e48e34f023ceb976b54ad04df40a3745a82d210
wata41 <130279514+wata41@users.noreply.github.com>
2023-05-19 09:58:29 +0000

    7.18-7.22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
     @user = User.new
       # => GET app/views/users/new.html.erb
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
