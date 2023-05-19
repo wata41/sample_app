28ab7c40397ea9388fc797bc467cfdff017f5126
wata41 <130279514+wata41@users.noreply.github.com>
2023-05-19 11:50:49 +0000

    8.9-8.19

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,15 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # POST /login
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
