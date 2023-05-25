2ad357d49151e054d4df80cb6ef78712e2334f47
wata41 <130279514+wata41@users.noreply.github.com>
2023-05-25 18:09:29 +0000

    9.22-9.26

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session      # ログインの直前に必ずこれを書くこと
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
