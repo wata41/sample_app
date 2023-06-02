f3ec5b391535fd9d4334774612a39b27f21ef065
wata41 <130279514+wata41@users.noreply.github.com>
2023-06-02 08:44:00 +0000

    13.12-13.22

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,8 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup 
     @user = users(:michael)
-    # 慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -25,6 +24,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 
 end
